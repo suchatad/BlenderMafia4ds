79ff8aec49fb10f7d7825526c7517bd6733c8dcd
suchatad <110851759+suchatad@users.noreply.github.com>
2022-09-05 22:13:44 +0200

    Parser fixes

--- mafia_4ds/parse_4ds.py
+++ mafia_4ds/parse_4ds.py
@@ -1,42 +1,51 @@
-__all__ = (
-    "parse",
-    "4DSElem",
-)
-
 from struct import unpack
+
 
 def read_ushort(reader):
     return unpack(b'<H', reader.read(2))[0]
 
+
 def read_uint(reader):
     return unpack(b'<I', reader.read(4))[0]
+
 
 def read_uint64(reader):
     return unpack(b'<Q', reader.read(8))[0]
 
+
 def read_float(reader):
-    return unpack(b'<I', reader.read(4))[0]
+    return unpack(b'<f', reader.read(4))[0]
+
 
 def read_doublet(reader):
     return unpack("fff", reader.read(4 * 2))
 
+
 def read_triplet(reader):
     return unpack("fff", reader.read(4 * 3))
+
 
 def read_ubyte(reader):
     return unpack(b'B', reader.read(1))[0]
 
+
 def read_string_fixed(reader,length):
-    bytes = reader.read(4)
+    bytes = reader.read(length)
 
     if bytes[-1] != 0:
         raise ValueError('Not a NULL terminated string.')
 
-    string = bytes[:-1].decode('utf-8') #TODO: handle exception
+    string = bytes[:-1].decode('utf-8')  # TODO: handle exception
     return string
+
 
 class FourDSFile:
     pass
+
+
+class Material:
+    pass
+
 
 def parse(filename):
     pass
